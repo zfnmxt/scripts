bfdbcee75351e4f65386c7a23683e22af08b4e79
zfnmxt <zfnmxt.com>
2019-08-25 11:17:25 +0200

    Better

--- TurtleBook/src/Main.hs
+++ TurtleBook/src/Main.hs
@@ -5,10 +5,18 @@ module Main where
 import Turtle
 import Prelude hiding (FilePath)
 import Data.List as L hiding (find)
+import Data.Text.Metrics
+import Data.Either
 import Data.Maybe
 
+parser :: Parser (FilePath, FilePath)
+parser = (,) <$> argPath "src" "Book source directory"
+             <*> argPath "dest" "Book destination directory"
+
 main :: IO ()
-main = putStrLn "someFunc"
+main = do
+  (src, dest) <- options "A book copy utility" parser
+  copyBooks src dest
 
 matchExt :: Text -> Pattern Text
 matchExt ext = star anyChar <> text ext <> ((const "") <$> eof)
@@ -28,18 +36,13 @@ books :: FilePath -> IO [FilePath]
 books path = do
   bookPaths <- reduce (Fold (flip (:)) [] id) $ find extPat path
   return $ nubBy eqFileName $  L.sortBy sorter bookPaths
-  where extension' = fromMaybe (error "oops") . extension
+  where extension' = fromMaybe (error "should never happen") . extension
         priority = zipWith (,) exts [1..]
         sorter p1 p2 = let e1 = extension' p1; e2 = extension' p2
                        in lookup e1 priority `compare` lookup e2 priority
-        eqFileName p1 p2
-         | length f1 == length f2 = ratio > threshold
-         | otherwise = f1 == f2
-           where matches = sum $ map (\b -> if b then 1 else 0) $ zipWith (==) f1 f2
-                 ratio = (fromIntegral . toInteger) matches / (fromIntegral . toInteger) (length f1)
-                 threshold = 0.85 :: Double
-                 f1 = encodeString $ dropExtension p1
-                 f2 = encodeString $ dropExtension p2
+        eqFileName p1 p2 = damerauLevenshteinNorm f1 f2 > 0.95
+           where f1 = fromRight (error "invalid path") $ toText $ dropExtension p1
+                 f2 = fromRight (error "invalid path") $ toText $ dropExtension p2
 
 copyBooks :: FilePath -> FilePath -> IO ()
 copyBooks src dest = do paths <- books src
